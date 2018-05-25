745939bfcb1a988a2aeaf9f3900f99016fa5d28a
ssy828 <828yss@gmail.com>
2018-05-25 14:20:59 +0900

    modified tableview static cells

--- ContainerVC/ContainerVC/TodayPart/View/CustomHeaderView.swift
+++ ContainerVC/ContainerVC/TodayPart/View/CustomHeaderView.swift
@@ -30,14 +30,4 @@ class CustomHeaderView: UITableViewHeaderFooterView {
     static var identifier: String {
         return String(describing: self)
     }
-    
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
