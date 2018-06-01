f805550fa847fca6dccbcfafb3519206efca93e1
ssy828 <828yss@gmail.com>
2018-06-01 20:43:12 +0900

    modifying operator Button How can I use only the enum type values I have defined?

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -10,7 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // MARK: enum
+    // MARK: Properties
+    private var display = String()
+    private var leftOperand: Double?
+    private var isOperatorClicked: Bool = false // 연속으로 연산자 출력 못하기 위해서
+    private var runningValue : String {
+            return !(display.isEmpty) ? display : "0"
+        }
+//    private var runningValue = String()
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var displayLB: UILabel!
+    
+    // MARK: - IBAction
+    @IBAction func operatorButton(_ sender: CustomRoundButton) {
+        guard let operatorValue = sender.titleLabel?.text else { return }
+        isOperatorClicked = !isOperatorClicked
+        NSLog("\(self.isOperatorClicked)")
+        if !(sender.tag == 17) {
+            self.leftOperand = Double(display)
+        } else {
+            if isOperatorClicked && sender.tag == 17{
+                if let operand = self.leftOperand, let rightOperand = Double(runningValue) {
+                    let `operator` = Operator.init(rawValue: operatorValue)
+                    if let result = `operator`?.operation(left: operand, right: rightOperand) {
+                        NSLog("\(result)")
+                    }
+                }
+                
+            }
+        }
+        isOperatorClicked = true
+    }
+    @IBAction func equalButton(_ sender: CustomRoundButton) {
+        
+    }
+    
+    @IBAction func doClickNumberButton(_ sender: CustomRoundButton){
+        guard let input = sender.titleLabel?.text else { return }
+        if isOperatorClicked { // Save value when Operator button pressed
+           // single digit
+            display = input
+            NSLog(runningValue)
+            isOperatorClicked = false
+        } else {
+            // multiple digits
+            display += input
+        }
+        NSLog(display)
+        self.displayLB.text = display
+    }
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: - enum
     enum Operator: String {
         case addtion = "+"
         case subtraction = "-"
@@ -27,54 +84,5 @@ class ViewController: UIViewController {
         }
         
     }
-    
-    // MARK: Properties
-    private var display = String()
-    private var leftOperand: Double?
-    private var operatorValue: String?
-    private var operatorInstance: Operator?
-    private var isOperatorClicked: Bool = false // 연속으로 연산자 출력 못하기 위해서
-
-    // MARK: - IBOutlet
-    @IBOutlet weak var displayLB: UILabel!
-    
-    // MARK: - IBAction
-    @IBAction func operatorButton(_ sender: CustomRoundButton) {
-        self.operatorValue = sender.titleLabel?.text
-        isOperatorClicked = !isOperatorClicked
-        if let sign = operatorValue, let leftOperand = Double(display), let rightOperand = Double(display) {
-            let test = Operator.init(rawValue: sign)
-            if let result = test?.operation(left: leftOperand, right: rightOperand){
-                NSLog("\(result)")
-            }
-            
-        }
-//        isOperatorClicked = true
-    }
-    @IBAction func equalButton(_ sender: CustomRoundButton) {
-        
-    }
-    
-    @IBAction func doClickNumberButton(_ sender: CustomRoundButton){
-        if let input = sender.titleLabel?.text {
-            if isOperatorClicked {
-                // single digit
-                display = input
-                isOperatorClicked = false
-            }else {
-                // multiple digits
-                display += input
-            }
-        }
-        self.displayLB.text = display
-    }
-    
-    // MARK: - Life Cycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    
 }
 
