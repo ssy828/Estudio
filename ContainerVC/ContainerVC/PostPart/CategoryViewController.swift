3d63e4c16cc4a4d5644f1af9aa711a44100c564e
ssy828 <828yss@gmail.com>
2018-07-16 14:45:57 +0900

    button image changes with button click

--- ContainerVC/ContainerVC/PostPart/CategoryViewController.swift
+++ ContainerVC/ContainerVC/PostPart/CategoryViewController.swift
@@ -9,37 +9,44 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
-
-    // MARK: Properties
     
+    // MARK: Properties
+    var isClicked: Bool = true // 버튼 눌렀을 경우
     // MARK: IBOutlet
     
     // MARK: IBAction
     @IBAction func didTapColorButton(_ sender: UIButton) {
-        sender.setImage(#imageLiteral(resourceName: "icons8-checkmark-24"), for: UIControlState.normal)
+        if isClicked {
+            sender.setImage(#imageLiteral(resourceName: "icons8-checkmark-24"), for: .normal)
+            isClicked = !isClicked
+        }else{
+            sender.setImage(nil, for: .normal)
+            isClicked = !isClicked
+        }
+        
     }
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
