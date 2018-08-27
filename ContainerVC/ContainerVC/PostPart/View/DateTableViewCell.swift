3fe3704274cdcf6206d1072dbce044c591618417
ssy828 <828yss@gmail.com>
2018-08-27 16:18:43 +0900

    add DateTableViewCell

---
+++ ContainerVC/ContainerVC/PostPart/View/DateTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DateTableViewCell.swift
+//  ContainerVC
+//
+//  Created by SSY on 2018. 8. 22..
+//  Copyright © 2018년 LittleMe. All rights reserved.
+//
+
+import UIKit
+
+class DateTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
