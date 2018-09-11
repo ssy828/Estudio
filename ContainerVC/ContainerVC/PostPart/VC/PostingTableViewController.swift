0f72b82f8640d54ccea0645a9d1473019b61f7db
ssy828 <828yss@gmail.com>
2018-09-11 13:21:16 +0900

    display datepicker date in another cell label

--- ContainerVC/ContainerVC/PostPart/VC/PostingTableViewController.swift
+++ ContainerVC/ContainerVC/PostPart/VC/PostingTableViewController.swift
@@ -29,26 +29,26 @@ class PostingTableViewController: UITableViewController {
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 3
     }
-
+    // MARK: numberOfRowsInSection
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         switch section {
@@ -59,9 +59,9 @@ class PostingTableViewController: UITableViewController {
         default:
             return 1
         }
-       
+        
     }
-
+    // MARK: cellForRowAt
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch (indexPath.section, indexPath.row) {
         case (0,0):
@@ -72,16 +72,20 @@ class PostingTableViewController: UITableViewController {
             return priceCell
         case (1,0):
             let colorPickerCell = tableView.dequeueReusableCell(withIdentifier: "ColorPickerCell", for: indexPath) as! ColorPickerTableViewCell
-            colorPickerCell.didAddHandler = { (color,text) in
+            colorPickerCell.didAddHandler = { [weak self] (color,text) in
+                guard let `self` = self else {return}
                 self.color = color
                 self.colorText = text
             }
             return colorPickerCell
         case (1,1):
-            let dateDetailCell = tableView.dequeueReusableCell(withIdentifier: "DateCell")
-            return dateDetailCell!
+            if let dateCell = tableView.dequeueReusableCell(withIdentifier: "DateCell") {
+                return dateCell
+            }
+            return UITableViewCell()
         case (1,2):
-            let datePickerCell = tableView.dequeueReusableCell(withIdentifier: "DatePickerCell", for: indexPath) as! DatePickerTableViewCell
+            let datePickerCell = tableView.dequeueReusableCell(withIdentifier: "DatePickerCell", for: indexPath) as! DateTableViewCell
+            datePickerCell.delegate = self
             return datePickerCell
         case (2,0):
             let textviewCell = tableView.dequeueReusableCell(withIdentifier: "TextViewCell", for: indexPath) as! TextViewTableViewCell
@@ -90,61 +94,69 @@ class PostingTableViewController: UITableViewController {
             return UITableViewCell()
         }
     }
-    
+    // MARK: heightForRowAt
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         switch (indexPath.section,indexPath.row) {
         case (1,2),(2,0):
             return 100
-//        case (2,0):
-//            return 100
         default:
             return 45
         }
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: - DateTableViewCellDelegate
+extension PostingTableViewController: DateTableViewCellDelegate {
+    func pass(date: String) {
+        // To input date in detailTextLabel of cell
+        let cell = self.tableView.cellForRow(at: IndexPath(row: 1, section: 1))
+        // 지정된 부분으로 셀 반환!
+        cell?.detailTextLabel?.text = date
     }
-    */
-
 }
--- ContainerVC/ContainerVC/PostPart/View/DateTableViewCell.swift
+++ ContainerVC/ContainerVC/PostPart/View/DateTableViewCell.swift
@@ -2,23 +2,33 @@
 //  DateTableViewCell.swift
 //  ContainerVC
 //
-//  Created by SSY on 2018. 8. 22..
+//  Created by SSY on 2018. 9. 10..
 //  Copyright © 2018년 LittleMe. All rights reserved.
 //
 
 import UIKit
+// MARK: - DateTableViewCellDelegate
+protocol DateTableViewCellDelegate: class {
+    func pass(date: String)
+}
 
 class DateTableViewCell: UITableViewCell {
+
+    // MARK: properties
+    weak var delegate: DateTableViewCellDelegate?
+    // MARK: IBOutlet
+    @IBOutlet weak var datePicker: UIDatePicker!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    @IBAction func didPickDate(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "MM월dd일 EEEE"
+        let currentDate = dateFormatter.string(from: sender.date)
+        self.delegate?.pass(date: currentDate)
     }
 
 }
